7b5fa56cd7bf692da5d1d200b7a43f19035018df
Aryman Reis <arymanreis@gmail.com>
2024-04-13 11:54:24 -0300

    gerador de senha
    gerador de senha consluído

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
-console.log("vamos vencer");
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Função
+const getLetterLowerCase = () => {
+
+    return (String.fromCharCode(Math.floor(Math.random() * 26 + 97)));
+}
+
+const getLetterUpperCase = () => {
+
+    return (String.fromCharCode(Math.floor(Math.random() * 26 + 65)));
+}
+
+const getNunber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const Symbols = "#$%&*=_-!?^{}[],;"
+    return Symbols[Math.floor(Math.random() * Symbols.length)];
+}
+
+console.log(getSymbol());
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNunber, getSymbol) => {
+
+    let password = ""
+
+    const passwordLength = 10
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNunber,
+        getSymbol
+    ]
+
+    for(i = 0; i < passwordLength; i = i + 4) {
+
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        });
+    }
+    password = password.slice(0,passwordLength);
+
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+};
+
+
+
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNunber,
+        getSymbol
+    );
+});
+
